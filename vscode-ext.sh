59600dbf7d2a5834b9e9f5ce10ffda5cb4e6f905
Daniel Alvarez <daniel@alvarez.tech>
2023-04-05 19:04:48 -0700

    Improvements

--- vscode-ext.sh
+++ vscode-ext.sh
@@ -3,7 +3,10 @@ NC='\033[0m' # No Color
 printf "${GREEN}VS Code Extensions:${NC}\n"
 code --list-extensions
 printf "${GREEN}...${NC}\n"
+
 code --install-extension unifiedjs.vscode-mdx --force
-code --install-extension yzhang.markdown-all-in-one --force
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension agenric.cocoapods-snippets --force
+
 printf "${GREEN}VS Code Extensions Now:${NC}\n"
 code --list-extensions
